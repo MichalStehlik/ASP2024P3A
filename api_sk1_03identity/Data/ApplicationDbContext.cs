48d0446f64a8340f29098069a589efcafba31455
Michal Stehlík <michal.stehlik@pslib.cz>
2024-11-13 11:42:34 +0100

    identity

---
+++ api_sk1_03identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using api_sk1_03identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_sk1_03identity.Data
+{
+    public class ApplicationDbContext: IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ api_sk1_03identity/Models/Note.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace api_sk1_03identity.Models
+{
+    public class Note
+    {
+        public int NoteId { get; set; }
+        public required string Text { get; set; }
+        public required string UserId { get; set; }
+        public IdentityUser? User { get; set; }
+    }
+}
